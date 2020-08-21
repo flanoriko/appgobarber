63924bc4899e4716e22bd46f1c90dc59ed51e9f7
Flavia Fukuda <flavia.noriko.work@gmail.com>
2020-08-21 11:11:03 -0300

    fonts

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,9 +1,15 @@
+import logoImg from '../../assets/logo.png';
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
 const Signin: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
 
 export default Signin;
